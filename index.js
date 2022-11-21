733e5574581d841528713242d6b1cf81e59ace4e
Tylerjaydenne Anthony <tylerjaydenne127@gmail.com>
2022-11-21 13:49:58 -0500

    commit tree fec28f2e6dc385100e340a0881d2c14f923a5027

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event){
+  console.log("names", names)
+  let newArray = []
+  for (let i=0; i < names.length; i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+function countDown(n){
+   for (let i=n; i >= 0; i--) {
+    console.log(i);
+} 
+ }
